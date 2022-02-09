1393b843ec68ba54a1de0f38b11ef6268b20cada
denstiny <2254228017@qq.com>
2022-02-09 12:55:49 +0800

    nvim and font

---
+++ fonts/install.sh
@@ -0,0 +1,11 @@
+# @author      : aero (2254228017@qq.com)
+# @file        : install
+# @created     : 星期三 2月 09, 2022 12:51:21 CST 
+# @github      : https://github.com/denstiny
+# @blog        : https://denstiny.github.io
+
+#!/bin/bash
+# 下载字体
+sudo pacman -S noto-fonts-cjk
+# 设置字体
+cp font.conf /etc/fonts/conf.d/fonts.conf
