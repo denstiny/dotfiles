6aaa844d678a2bad241e47f3670a84b686d95921
denstiny <2254228017@qq.com>
2022-02-12 15:58:52 +0800

    update fish and rofi

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    set -U fish_greeting ""
+    # Commands to run in interactive sessions can go here
+    clear
+    export EDITOR=/bin/nvim
+    alias ters='python /mnt/home/Terslation/translation/fanyim.py'
+    alias screenkey='screenkey -p fixed -g  (slop -n -f '%g')'
+    alias nvim='export LANG="en_US.UTF-8" && /usr/bin/nvim'
+    alias re=ranger
+end
