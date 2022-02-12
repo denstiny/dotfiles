8e1418e313562bb930cd25e5ee04ca80272355b3
denstiny <2254228017@qq.com>
2022-02-12 16:41:28 +0800

    grub

---
+++ grub/install.sh
@@ -0,0 +1,9 @@
+# @author      : aero (2254228017@qq.com)
+# @file        : install
+# @created     : 星期六 2月 12, 2022 16:40:32 CST 
+# @github      : https://github.com/denstiny
+# @blog        : https://denstiny.github.io
+
+#!/bin/bash
+sudo cp -r catppuccin-grub-theme /usr/share/grub/themes/
+sudo nvim /etc/default/grub
