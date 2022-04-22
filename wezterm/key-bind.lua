68be6d6793fad6301ca2ccbfeca16a0cfb7777c7
denstiny <2254228017@qq.com>
2022-04-22 14:55:48 +0800

    fead: wezterm 配置文件

---
+++ wezterm/key-bind.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm';
+function keyBind()
+    keys = {
+    {key="k", mods="CTRL|SHIFT", action=wezterm.action{ScrollByPage=-1}},
+    {key="j", mods="CTRL|SHIFT", action=wezterm.action{ScrollByPage=1}},
+    {key="k", mods="CTRL", action=wezterm.action{ScrollByLine=-1}},
+    {key="j", mods="CTRL", action=wezterm.action{ScrollByLine=1}},
+    {key="f", mods="CTRL", action=wezterm.action{Search={CaseSensitiveString=""}}},
+        {key = "=", mods = "CTRL", action = "ResetFontSize"},
+    {key = "+", mods = "CTRL", action = "IncreaseFontSize"},
+    {key = "-", mods = "CTRL", action = "DecreaseFontSize"}
+    }
+  return keys
+end
+
