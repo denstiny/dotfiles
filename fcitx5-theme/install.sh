290b8ffa86fd48d75c28e54a280c79451faa35f2
denstiny <2254228017@qq.com>
2023-03-08 18:07:04 +0800

    add: fcitx5 config

---
+++ fcitx5-theme/install.sh
@@ -0,0 +1,9 @@
+# @author      : aero (2254228017@qq.com)
+# @file        : install
+# @created     : 星期三 3月 08, 2023 13:36:54 CST 
+# @github      : https://github.com/denstiny
+# @blog        : https://denstiny.github.io
+
+#!/usr/bin/env bash
+
+cp -rf ./Black-Simplicity ~/.local/share/fcitx5/themes/
